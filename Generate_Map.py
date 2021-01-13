b5b0979d6abcaf304a242259d173cadbde9aa2c4
Michael <michael.morris.london@gmail.com>
2021-01-13 21:05:54 +0000

    added ode solver - not tuned

--- Generate_Map.py
+++ Generate_Map.py
@@ -6,7 +6,10 @@ from matplotlib.ticker import MaxNLocator
 import numpy as np
 
 class map_2d():
-    def __init__(self, size = [50,50], xlim = [0,10], ylim = [0,10]):
+    def __init__(self, size = [80,80], xlim = [0,10], ylim = [0,10]):
+        self.xlim = xlim
+        self.ylim = ylim
+
         self.size_x = size[0]
         self.size_y = size[0]
 
@@ -17,19 +20,18 @@ class map_2d():
         self.dy = (ylim[1] - ylim[0]) / size[1]
         self.dx = (xlim[1] - xlim[0]) / size[0]
 
+        self.d_min = min(self.dx, self.dy)
+
         # generate 2 2d grids for the x & y bounds
-        self.y, self.x = np.mgrid[slice(ylim[0], ylim[1] + self.dy, self.dy),
-                                  slice(xlim[0], xlim[1] + self.dx, self.dx)]
+        self.y, self.x = np.meshgrid(np.arange(xlim[0], xlim[1]+self.dx, self.dx),
+                                     np.arange(ylim[0], xlim[1]+self.dy, self.dy))
 
-
-
-        # self.z = np.sin(self.x) ** 10 + np.cos(10 + self.y * self.x) * np.cos(self.x)
         self.z = np.zeros(self.x.shape)
 
         return None
 
     def fill_map(self):
-        n_shapes = 25
+        n_shapes = 10
         size_of_shapes = 10
 
         for _ in range(n_shapes):
@@ -51,8 +53,8 @@ class map_2d():
         # contours are *point* based plots, so convert our bound into point
         # centers
 
-        cf = ax0.contourf(self.x + self.dx / 2.,
-                          self.y + self.dy / 2., to_plot)
+        cf = ax0.contourf(self.x,
+                          self.y, to_plot)
 
 
         fig.colorbar(cf, ax=ax0)
@@ -65,105 +67,110 @@ class map_2d():
         plt.show()
 
     def get_f(self, pos):
-        x = pos[0]
-        y = pos[1]
-        dx = []
-        dy = []
-        for i in range(self.size_x+1):
-            dy.append(i - x)
-            # dx.append(x - i)
+        dx = pos[0] - self.x
+        dy = pos[1] - self.y
 
-
-        for j in range(self.size_y+1):
-            dx.append(j - y)
-            # dy.append(y - j)
-
-        dx = np.asarray(dx)[np.newaxis, :]
-        dy = np.asarray(dy)[:, np.newaxis]
+        self.dxdy = np.maximum(np.square(dx) + np.square(dy) , self.d_min)
 
         thetas = np.arctan2(dx, dy)
-        self.dxdy = (np.tile(np.square(dx), (51, 1)) + np.tile(np.square(dy), (1, 51))) + 1e-5
 
         self.f_map = np.divide(self.z, self.dxdy)
-        temp = np.minimum(self.f_map, np.zeros((51,51))+0.5) + 1e-5
-        tot = temp.sum()
 
+        fx = np.multiply(np.sin(thetas), self.f_map).sum()
+        fy = np.multiply(np.cos(thetas), self.f_map).sum()
 
-        # self.f_map = self.f_map / self.f_map.max()
-        # temp = np.minimum(self.f_map, np.zeros((51, 51)) + 0.5)
-        fx = np.multiply(np.sin(thetas), temp).sum()
-        fy = np.multiply(np.cos(thetas), temp).sum()
+        return fx, fy, self.f_map.sum()
 
-        return fx, fy, tot
+class pos_solv():
+    def __init__(self, start = np.array([5,0]), end = np.array([5,10])):
+        self.pos_hist = [start]
+
+        self.pos = start
+        self.end = end
+
+        self.dxdy = np.sqrt(np.square(start - end).sum())
+        self.tol = 1
+
+        self.mass = 15
+
+        self.f_goal = 20
+        self.vel = np.array([0,0])
+        self.vel_hist = [self.vel]
+
+    def get_f(self, map):
+        fx, fy, _ = map.get_f(self.pos)
+
+        self.dxdy = self.end - self.pos
+
+        theta = np.arctan2(self.dxdy[1], self.dxdy[0])
+
+        fx_goal = self.f_goal * np.cos(theta)
+        fy_goal = self.f_goal * np.sin(theta)
+
+        fx = fx + fx_goal
+        fy = fy + fy_goal
+
+        return np.asarray([fx, fy])
+
+    def next_pos(self, map, dt = 0.005):
+        force = self.get_f(map)
+
+        self.vel = self.vel + force / self.mass * dt
+        self.pos = self.pos + self.vel * dt
+
+        self.vel_hist.append(self.vel)
+        self.pos_hist.append(self.pos)
+
+    def terminal(self):
+        dist = np.sqrt(np.square(self.pos - self.end).sum())
+
+        if dist < self.tol:
+            return True
+        return False
 
 map = map_2d()
+pos_ode = pos_solv()
 map.fill_map()
-map.plot(map.z)
+map.plot(map.z, 'map')
 
+for i in range(10000):
+    pos_ode.next_pos(map)
+    if pos_ode.terminal():
+        break
 
-pos = np.multiply([9,3], 5)
-pos_hist = []
-
-f_all = np.zeros((51,51))
-fx_all = np.zeros((51,51))
-fy_all = np.zeros((51,51))
-for i in range(51):
-    for j in range(51):
-        fx, fy, f = map.get_f([i,j])
-        f_all[i,j] = f
-        fx_all[i, j] = fx
-        fy_all[i, j] = fy
-
-map.plot(fx_all / fx_all.max(), 'fx')
-map.plot(fy_all / fy_all.max(), 'fy')
-map.plot(np.sqrt(f_all) / np.sqrt(f_all).max(), 'f_total')
-
-# for i in range(25):
-#
-#     fx, fy = map.get_f(pos)
-#     pos = (pos + np.asarray([fx, fy])).round().astype(int)
-#     map.plot_dist(pos)
-#     pos_hist.append(pos)
+plt.subplot(2,1,1)
+plt.title('position')
+plt.plot(np.asarray(pos_ode.pos_hist)[:,0], np.asarray(pos_ode.pos_hist)[:,1])
+plt.xlim([0,10])
+plt.ylim([0,10])
+plt.subplot(2,1,2)
+plt.title('velocity')
+plt.plot(np.asarray(pos_ode.vel_hist)[:,0], np.asarray(pos_ode.vel_hist)[:,1])
+plt.xlim([-10,10])
+plt.ylim([-10,10])
+plt.show()
 
 
 
 
 
-# # make these smaller to increase the resolution
-# dx, dy = 0.05, 0.05
-#
-# # generate 2 2d grids for the x & y bounds
-# y, x = np.mgrid[slice(1, 5 + dy, dy),
-#                 slice(1, 5 + dx, dx)]
-#
-# z = np.sin(x)**10 + np.cos(10 + y*x) * np.cos(x)
-#
-# # x and y are bounds, so z should be the value *inside* those bounds.
-# # Therefore, remove the last value from the z array.
-# z = z[:-1, :-1]
-# levels = MaxNLocator(nbins=15).tick_values(z.min(), z.max())
-#
-#
-# # pick the desired colormap, sensible levels, and define a normalization
-# # instance which takes data values and translates those into levels.
-# cmap = plt.get_cmap('PiYG')
-# norm = BoundaryNorm(levels, ncolors=cmap.N, clip=True)
-#
-# fig, ax0,  = plt.subplots(1,1)
-#
-#
-#
-#
-# # contours are *point* based plots, so convert our bound into point
-# # centers
-# cf = ax0.contourf(x[:-1, :-1] + dx/2.,
-#                   y[:-1, :-1] + dy/2., z, levels=levels,
-#                   cmap=cmap)
-# fig.colorbar(cf, ax=ax0)
-# ax0.set_title('contourf with levels')
-#
-# # adjust spacing between subplots so `ax1` title and `ax0` tick labels
-# # don't overlap
-# fig.tight_layout()
-#
+# f_all = np.zeros(map.x.shape)
+# fx_all = np.zeros(map.x.shape)
+# fy_all = np.zeros(map.x.shape)
+
+# for idx_x, x in enumerate(np.arange(map.xlim[0], map.xlim[1] + map.dx, map.dx)):
+#     for idx_y, y in enumerate(np.arange(map.ylim[0], map.xlim[1] + map.dy, map.dy)):
+#         fx, fy, f = map.get_f([x,y])
+#         f_all[idx_x, idx_y] = f
+#         fx_all[idx_x, idx_y] = fx
+#         fy_all[idx_x, idx_y] = fy
+
+# map.plot(fx_all, 'fx')
+# map.plot(fy_all, 'fy')
+# map.plot(f_all, 'f_total')
+
+# fig, ax = plt.subplots()
+# q = ax.quiver(map.x, map.y, fx_all, fy_all)
+# ax.quiverkey(q, X=0.3, Y=1.1, U=10,
+#              label='Quiver key, length = 10', labelpos='E')
 # plt.show()
